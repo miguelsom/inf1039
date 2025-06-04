a9da317f354530e8a7e5ee7762376e6ff308e57c
TomasPegado <lenzitomas00@gmail.com>
2025-06-04 08:43:59 -0300

    fix: base.html and static files location

--- GradGuru/account/views.py
+++ GradGuru/account/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from .forms import LoginForm, UserRegistrationForm, \
-                    UserEditForm, ProfileEditForm, ProfileForm
+                    UserEditForm, ProfileEditForm
 from .models import Profile
 
 
